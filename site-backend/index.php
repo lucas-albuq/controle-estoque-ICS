9e1cc1e1c3437b2230e82a6f4c96ec311f0fb78d
Lucas Francisco de Albuquerque Barbosa <luuinhasalbuquerque@gmail.com>
2025-01-22 09:28:44 -0300

    Update index.php

--- site-backend/index.php
+++ site-backend/index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <div class="container mt-5">
-        <h1>Controle de Estoque</h1>
+        <h1 class="mb-4">Controle de Estoque</h1>
         <a href="views/produtos.php" class="btn btn-primary">Gerenciar Produtos</a>
         <a href="views/movimentacoes.php" class="btn btn-secondary">Gerenciar Movimentações</a>
     </div>
