35830a7a826e1f2116cb722b0b95b5a6f0e04815
Lucas Francisco de Albuquerque Barbosa <luuinhasalbuquerque@gmail.com>
2025-02-26 13:32:20 -0300

    Update movimentacoes.php

--- site-backend/views/movimentacoes/movimentacoes.php
+++ site-backend/views/movimentacoes/movimentacoes.php
@@ -54,6 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <title>Gerenciar Movimentações</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
